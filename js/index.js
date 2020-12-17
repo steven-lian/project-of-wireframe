0857f1e38ca2dd9c749db2acd0b844b4e56e936f
Sheng <lianshengsteven@gmail.com>
2020-12-17 18:21:21 +1100

    add javascript

---
+++ js/index.js
@@ -0,0 +1,117 @@
+// import createTaskHtml function from './taskManager';
+import {createTaskHtml} from './taskManager.js';
+
+const taskHtml = createTaskHtml ('name','description','assignedTo','dueDate','status')
+
+console.log(taskHtml);
+
+// import TaskManager class from './taskManager';
+
+import {TaskManager} from './taskManager.js';
+
+//create two new instances to test 
+const taskManager = new TaskManager(0);
+// console.log(taskManager.tasks);
+
+// const taskManager2 = new TaskManager(0);
+// taskManager2.addTasks("sgbvv","hhh","jjj","hgh","cc");
+// console.log(taskManager2.tasks);
+
+
+// select formFieldInput form
+const formFieldInput = document.querySelector("#formFieldInput");
+
+//add an onsubmit event listener to the form tag
+formFieldInput.addEventListener("submit",(event) => {
+
+    //prevent default action of refreshing page when submitting form
+    event.preventDefault();
+    // event.stopPropagation();
+
+
+
+    //select input
+    const formFieldName = document.querySelector("#formFieldName");
+    const formFieldDescription = document.querySelector("#formFieldDescription");
+    const formFieldAssigned = document.querySelector("#formFieldAssigned");
+    const formFieldDue = document.querySelector("#formFieldDue");
+    const formFieldStatus = document.querySelector("#formFieldStatus");
+    const errorMessage = document.querySelector("#alertMessage");
+    errorMessage.innerHTML = "";
+
+    // get the value of input
+
+    const name = formFieldName.value;
+    const description= formFieldDescription.value;
+    const assigned = formFieldAssigned.value;
+    const due = formFieldDue.value;
+    const status = formFieldStatus.value;
+
+    // validation code
+
+    function validFormFieldInput(){
+        // let name = document.forms["formFieldInput"]["formFieldName"].value;
+        if(name===""){
+            errorMessage.innerHTML += "Invalid name input; " ;
+           
+        }
+
+        // let description = document.forms["formFieldInput"]["formFieldDescription"].value;
+        if(description===""){
+            errorMessage.innerHTML += "Invalid description input; ";
+           
+        }
+
+        // let assigned = document.forms["formFieldInput"]["formFieldAssigned"].value;
+        if(assigned===""){
+            errorMessage.innerHTML += "Invalid assigned input; ";
+         
+        }
+
+        // let due = document.forms["formFieldInput"]["formFieldDue"].value;
+        if(due===""){
+            errorMessage.innerHTML += "Invalid due input; ";
+            
+        }
+
+        // let status = document.forms["formFieldInput"]["formFieldStatus"].value;
+        if(status===""){
+            errorMessage.innerHTML += "Invalid status input";
+            
+        }
+    };
+
+
+    // control the div with id named errorMessage in taskslist.html content display mathod
+
+    if(!validFormFieldInput(name,description,assigned,due,status)){
+        
+        errorMessage.style.display = "block"
+    }else{
+        errorMessage.style.display = "none";
+
+        
+    }
+
+    // Add all new task form input value to the task manager when click submit
+        
+        //  const newTaskSubmit = new TaskManager(0);
+         taskManager.addTasks(name,description,assigned,due,status);
+        //  console.log(newTaskSubmit.tasks);
+
+    // Render the tasks
+    taskManager.render();
+
+    // Clear the form input value after submitting 
+    formFieldName.value = '';
+    formFieldDescription.value = '';
+    formFieldAssigned.value = '';
+    formFieldDue.value = '';
+    formFieldStatus.value = '';
+
+        
+});
+
+// function validFormFieldInput(data){
+//     return data !== null && data !== '';
+// }
---
+++ js/taskManager.js
@@ -0,0 +1,81 @@
+// Create and export a createTaskHtml function 
+export const createTaskHtml = (name,description,assignedTo,dueDate,status) =>
+ 
+ `
+ <li class="list-group-item">
+        <div class="d-flex w-100 mt-2 justify-content-between align-items-center">
+            <h5>${name}</h5>
+            <span class="badge badge-danger">${status}</span>
+        </div>
+        <div class="d-flex w-100 mb-3 justify-content-between">
+            <small>Assigned To: ${assignedTo}</small>
+            <small>Due: ${dueDate}</small>
+        </div>
+        <p>${description}</p>
+    </li>
+`;
+
+
+
+
+
+// Create and export a TaskManager class 
+
+export class TaskManager {
+ 
+    //creat a empty array called tasks to store each new task object 
+    constructor(currentId = 0) {
+        this.tasks = [];
+        this.currentId = currentId;
+    }
+
+// create a method to add Task
+    addTasks(name,description,assignedTo,dueDate,status){
+    
+    //create a task object with self-increment id and input name 
+        const task = {
+        id : this.currentId ++,
+        name: name,
+        description: description,
+        assignedTo : assignedTo,
+        dueDate: dueDate,
+        status: status
+    };
+
+    // push each new task object to the 'tasks' array.
+        this.tasks.push(task);
+    
+  }
+
+ // Create the render method
+    render() {
+    // Create an array to store the tasks' HTML
+        const tasksHtmlList = [];
+
+    // Loop over our tasks and create the html, storing it in the array
+        for (let i = 0; i < this.tasks.length; i++) {
+        // Get the current task in the loop
+            const task = this.tasks[i];
+
+            // Format the date
+            const date = new Date(task.dueDate);
+            const formattedDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+
+            // Create the task html
+            const taskHtml = createTaskHtml(task.name, task.description, task.assignedTo, formattedDate, task.status);
+
+            // Push it to the tasksHtmlList array
+            tasksHtmlList.push(taskHtml);
+        }
+
+    // Create the tasksHtml by joining each item in the tasksHtmlList
+    // with a new line in between each item.
+    const tasksHtml = tasksHtmlList.join('\n');
+
+    // Set the inner html of the tasksList on the page
+    const tasksList = document.querySelector('#tasksList');
+    tasksList.innerHTML = tasksHtml;
+    }
+  
+}
+
